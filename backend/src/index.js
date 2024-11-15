be2ddb59257b29b6c4cb0115769135966722095e
ErvinCaraval <ervin.caravali@correounivalle.edu.co>
2024-11-15 03:41:44 -0500

    se conectaron preacticas,Trabajos de Grado y miembros del grupo al backend ahora el dash board y landing page estan conectadas  por el backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,18 +9,15 @@ import lineasInvestigacionRoutes from './routes/lineasInvestigacion.js';
 import calificacionGrupoRoutes from './routes/calificacionGrupo.js';
 import miembrosGrupoRoutes from './routes/miembrosGrupo.js';
 import conveniosAlianzasRoutes from './routes/conveniosAlianzas.js';
-import authRoutes from './routes/auth.js';
+import authRoutes from './routes/auth.js'; 
 
 const app = express();
 
-// Middleware para configurar CORS y limitar los métodos a solo GET
 app.use(cors({
     origin: ['http://localhost:5173', 'http://localhost:5273'],  // Agregar el nuevo puerto
-    methods: ['GET'],  // Limitar los métodos permitidos a GET
     credentials: true
 }));
 
-// Middleware para parsear el cuerpo de las solicitudes
 app.use(bodyParser.json());
 
 // Configuración de la sesión
@@ -30,37 +27,19 @@ app.use(session({
     saveUninitialized: true
 }));
 
-// Middleware para verificar si el usuario está autenticado
-function ensureAuthenticated(req, res, next) {
-    const allowedOrigins = ['http://localhost:5173']; // Orígenes permitidos
 
-    // Verificar el origen de la solicitud
-    const origin = req.headers.origin;
-
-    if (allowedOrigins.includes(origin)) {
-        // Si el origen es permitido, permitir el acceso sin autenticación
-        return next();
-    }
-
-    // Verificar si el usuario está autenticado
-    if (req.session.user) {
-        return next();
-    }
-
-    res.status(401).json({ message: 'Debe estar autenticado para acceder a esta ruta' });
-}
 
 // Rutas de autenticación
 app.use('/auth', authRoutes);
 
 // Rutas protegidas por autenticación
-app.use('/practicas', ensureAuthenticated, practicasRoutes);
-app.use('/trabajosGrado', ensureAuthenticated, trabajosGradoRoutes); //desactivación de autenticación momentanea
-app.use('/proyectosInvestigacion', ensureAuthenticated, proyectosInvestigacionRoutes);
-app.use('/lineasInvestigacion', ensureAuthenticated, lineasInvestigacionRoutes);
-app.use('/calificacionGrupo', ensureAuthenticated, calificacionGrupoRoutes);
-app.use('/miembrosGrupo', ensureAuthenticated, miembrosGrupoRoutes); //desactivación de autenticación momentanea
-app.use('/conveniosAlianzas', ensureAuthenticated, conveniosAlianzasRoutes); //desactivación de autenticación momentanea
+app.use('/practicas',  practicasRoutes);
+app.use('/trabajosGrado', trabajosGradoRoutes); //desactivación de autenticación momentanea
+app.use('/proyectosInvestigacion', proyectosInvestigacionRoutes);
+app.use('/lineasInvestigacion', lineasInvestigacionRoutes);
+app.use('/calificacionGrupo', calificacionGrupoRoutes);
+app.use('/miembrosGrupo', miembrosGrupoRoutes); //desactivación de autenticación momentanea
+app.use('/conveniosAlianzas', conveniosAlianzasRoutes); //desactivación de autenticación momentanea
 
 // Inicializar el servidor
 const PORT = process.env.PORT || 3000;
